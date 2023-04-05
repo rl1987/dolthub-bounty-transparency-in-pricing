ece20e2aa85cd99532d6aaefd9e4a1d168f22a70
rl1987 <rl1987@users.noreply.github.com>
2023-04-05 13:35:20 +0700

    Add Terraform config with provisioning script

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = file("do_token.txt")
+}
+
+data "digitalocean_ssh_keys" "keys" {
+  sort {
+    key       = "name"
+    direction = "asc"
+  }
+}
+
+resource "digitalocean_droplet" "server" {
+  image     = "debian-11-x64"
+  name      = "dhdb-transparency-in-pricing"
+  region    = "sfo3"
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = [for key in data.digitalocean_ssh_keys.keys.ssh_keys : key.fingerprint]
+  user_data = file("provision.sh")
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    timeout     = "2m"
+    private_key = file("~/.ssh/id_ed25519")
+  }
+
+  provisioner "file" {
+    source      = "~/.dolt"
+    destination = "/root"
+  }
+
+  provisioner "file" {
+    source      = "./do_token.txt"
+    destination = "/root/do_token.txt"
+  }
+}
+
+output "server_ip" {
+  value = resource.digitalocean_droplet.server.ipv4_address
+}
+
---
+++ provision.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+
+apt-get update
+apt-get install -y python3 python3-pip tmux git vim visidata unzip jq
+
+curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh > /tmp/install.sh && bash /tmp/install.sh
+dolt config --global --add user.email rimantas@keyspace.lt
+dolt config --global --add user.name "rl1987"
+
+pip3 install --upgrade jupyterlab
+pip3 install --upgrade requests lxml doltpy scrapy openpyxl pandas xlrd visidata
+
+curl -sSL https://repos.insights.digitalocean.com/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
+
+mkdir /root/data
+
+pushd /root/data || exit
+dolt clone rl1987/paylesshealth
+dolt clone rl1987/transparency-in-pricing
+popd || exit
+
+swapoff -a
+dd if=/dev/zero of=/swapfile bs=1G count=16
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile swap swap sw 0 0" >> /etc/fstab
