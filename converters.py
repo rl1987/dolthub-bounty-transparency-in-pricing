98544427510fba5ab6cccb10e153377fe9afef85
rl1987 <rl1987@users.noreply.github.com>
2023-04-06 19:14:21 +0700

    Further work on Aurora transforms in response to feedback from spacelove

--- converters.py
+++ converters.py
@@ -20,17 +20,18 @@ class AbstractStandardChargesConverter(object):
 class AuroraXMLConverter(AbstractStandardChargesConverter):
     def __init__(self):
         super().__init__()
-    
+
     def convert(self, url, file_path, ccn):
         df_out = pd.DataFrame(columns=TARGET_COLUMNS)
-        
+
         df_in = pd.read_xml(file_path)
         # HACK: https://stackoverflow.com/a/50132405
-        df_in['NDC'] = df_in['NDC'].fillna('-1')
+        df_in['NDC'] = df_in['NDC'].fillna('')
         df_in['NDC'] = df_in['NDC'].astype(str)
-        df_in['Rev'] = df_in['Rev'].fillna(-1)
-        df_in['Rev'] = df_in['Rev'].astype(int)
-    
+        df_in['Rev'] = df_in['Rev'].fillna('nan')
+        df_in['Rev'] = df_in['Rev'].astype(str)
+        df_in['Chargecode_DRG_CPT'] = df_in['Chargecode_DRG_CPT'].astype(str)
+
         columns = df_in.columns.to_list()
         money_columns = list(filter(lambda c: c.startswith('_'), columns)) + ['Self_Pay', 'Min', 'Max']
 
@@ -46,27 +47,37 @@ class AuroraXMLConverter(AbstractStandardChargesConverter):
             'Description': 'procedure_desc',
             'CPT': 'hcpcs_cpt',
             'Rev': 'rev_code',
-            'Type': 'rev_desc',
             'NDC': 'ndc',
             'Chargecode_DRG_CPT': 'code'
         })
         del df_intermediate['Facility']
 
+        df_intermediate['rev_code'] = df_intermediate['rev_code'].apply(lambda rev_code: rev_code.split('.')[0])
+        df_intermediate['rev_code'] = df_intermediate['rev_code'].apply(pad_rev_code_if_needed)
         df_intermediate['hcpcs_cpt'] = df_intermediate['hcpcs_cpt'].fillna('-1')
         df_intermediate['hcpcs_cpt'] = df_intermediate['hcpcs_cpt'].astype(str)
         df_intermediate['hcpcs_cpt'] = df_intermediate['hcpcs_cpt'].replace('-1', '')
         df_intermediate['hcpcs_cpt'] = df_intermediate['hcpcs_cpt'].apply(lambda cpt: cpt[:5])
-        df_intermediate['rev_code'] = df_intermediate['rev_code'].replace(-1, 'nan')
-        df_intermediate['ndc'] = df_intermediate['ndc'].replace('-1', 'nan')
         df_intermediate['quantity_desc'] = 'nan'
-        df_intermediate['patient_class'] = df_intermediate['rev_desc'].replace(
+        df_intermediate['patient_class'] = df_intermediate['Type'].replace(
             'CHARGE', 'nan').replace(
             'IP DRG*', 'inpatient').replace(
             'OP PROC*', 'outpatient')
-        df_intermediate['code_type'] = df_intermediate['rev_desc'].replace(
+        df_intermediate['code_type'] = df_intermediate['Type'].replace(
             'IP DRG*', 'ms-drg').replace(
             'OP PROC*', 'hcpcs_cpt').replace(
-            'CHARGE', 'local')
+            'CHARGE', '')
+        df_intermediate['code_meta'] = df_intermediate['Type'].replace(
+            'IP DRG*', 'drg').replace(
+            'OP PROC*', 'cpt').replace(
+            'CHARGE', 'cdm')
+
+        del df_intermediate['Type']
+
+        # https://stackoverflow.com/a/60264415
+        df_intermediate['hcpcs_cpt'] = df_intermediate.apply(lambda row: row['code'] if row['code_meta'] == 'cpt' else None, axis=1)
+        df_intermediate['ms_drg'] = df_intermediate.apply(lambda row: row['code'] if row['code_meta'] == 'drg' else None, axis=1)
+        df_intermediate['cdm'] = df_intermediate.apply(lambda row: row['code'] if row['code_meta'] == 'cdm' else None, axis=1)
 
         def get_payer_category_from_payer_desc(payer_desc):
             if payer_desc == "Min":
@@ -77,11 +88,11 @@ class AuroraXMLConverter(AbstractStandardChargesConverter):
                 return "cash"
             elif payer_desc.endswith("_Fee"):
                 return "gross"
-            
+
             return "payer"
 
         df_intermediate['payer_category'] = df_intermediate['payer_desc'].apply(get_payer_category_from_payer_desc)
-        
+
         def get_payer_name_from_payer_desc(payer_desc):
             if 'Common_Ground' in payer_desc:
                 return 'Common Ground'
@@ -114,24 +125,24 @@ class AuroraXMLConverter(AbstractStandardChargesConverter):
             last_component = components[-1]
             if last_component in ["HMO", "PPO", "HPN", "GPPO", "EPO"]:
                 return last_component
-            
+
             return ''
 
         def get_plan_name_from_payer_desc(payer_desc):
             if payer_desc.endswith("Fee"):
                 return ''
-            
+
             payer_name = get_payer_name_from_payer_desc(payer_desc)
             components = payer_desc.split('_')
             components = components[2:]
             plan_type = get_plan_type_from_payer_desc(payer_desc)
             return ' '.join(components).replace(plan_type, '').replace(payer_name, '').strip()
-            
+
         df_intermediate['plan_type'] = df_intermediate['payer_desc'].apply(get_plan_type_from_payer_desc)
         df_intermediate['plan_name'] = df_intermediate['payer_desc'].apply(get_plan_name_from_payer_desc)
-        
+
         df_out = pd.DataFrame(columns=TARGET_COLUMNS)
 
         df_out = df_out.append(df_intermediate)
-        
+
         return df_out
