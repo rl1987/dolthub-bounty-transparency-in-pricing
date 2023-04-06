45464db58b97c8428ff2bbca0304ef35a406f2a4
rl1987 <rl1987@users.noreply.github.com>
2023-04-06 18:38:33 +0700

    Add helper function to pad a rev code if needed

--- helpers.py
+++ helpers.py
@@ -6,3 +6,14 @@ def cleanup_dollar_value(value):
 
 def cleanup_values(values):
     return list(map(lambda value: cleanup_dollar_value(value), values))
+
+def pad_rev_code_if_needed(rev_code):
+    if type(rev_code) == str:
+        if len(rev_code) == 3:
+            return '0' + rev_code
+        elif len(rev_code) == 2:
+            return '00' + rev_code
+        elif len(rev_code) == 1:
+            return '000' + rev_code
+
+    return rev_code
