447a361259ba114686e15800d6d41340ae2b68c0
rl1987 <rl1987@users.noreply.github.com>
2023-05-18 21:05:18 +0300

    Start new script to scrape CHS links

---
+++ chs_scrape_urls.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import csv
+
+import requests
+from lxml import html
+
+def main():
+    start_url = "https://www.chslocationsmap.com/data/map/default/locations.json?callback=locationDataCallback&_=1684433050008"
+
+if __name__ == "__main__":
+    main()
