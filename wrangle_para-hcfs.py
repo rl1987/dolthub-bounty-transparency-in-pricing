3ab5ed6cb8ab15318e0d8d66f056ca21f213bbb8
rl1987 <rl1987@users.noreply.github.com>
2023-06-10 18:24:29 +0300

    Further improvements to wrangle_para-hcfs.py

--- wrangle_para-hcfs.py
+++ wrangle_para-hcfs.py
@@ -52,11 +52,13 @@ def unify_modifiers(m1, m2):
     if m1 is None:
         return None
 
-    if m1 is not None:
-        if m2 is not None:
+    if m1 is not None and len(m1) == 2:
+        if m2 is not None and len(m2) == 2:
             return m1 + "|" + m2
 
         return m1
+
+    return None
 
 def payer_name_to_payer_category(payer_name):
     if payer_name == 'Discounted Cash Price':
@@ -89,15 +91,24 @@ def convert_chunk(chunk, ccn):
         'Diagnosis Related Group Description': 'description',
         'CPT HCPCS DRG Code': 'code',
         'Shoppable Services Code': 'local_code',
-        'Shoppable Services Description': 'description'
+        'Shoppable Services Description': 'description',
+        'Description': 'description',
     })
 
-    df_mid.loc[df_mid['modifiers'].isnull(), 'modifiers'] = None
-    df_mid.loc[df_mid['Modifier2'].isnull(), 'Modifier2'] = None
+    if 'modifiers' in df_mid.columns:
+        df_mid.loc[df_mid['modifiers'].isnull(), 'modifiers'] = None
+    else:
+        df_mid['modifiers'] = None
+    if 'Modifier2' in df_mid.columns:
+        df_mid.loc[df_mid['Modifier2'].isnull(), 'Modifier2'] = None
 
-    df_mid['modifiers'] = df_mid[['modifiers', 'Modifier2']].apply(lambda row: unify_modifiers(row['modifiers'], row['Modifier2']), axis=1)
-    del df_mid['Modifier2']
+    if 'modifiers' in df_mid.columns and 'Modifier2' in df_mid.columns:
+        df_mid['modifiers'] = df_mid[['modifiers', 'Modifier2']].apply(lambda row: unify_modifiers(row['modifiers'], row['Modifier2']), axis=1)
+        del df_mid['Modifier2']
+    else:
+        df_mid['modifiers'] = None
 
+    money_idx = df_mid.columns.to_list().index('Gross Charge')
     remaining_columns = df_mid.columns.to_list()[:8]
     df_mid = pd.melt(df_mid, id_vars=remaining_columns, var_name='payer_name', value_name='standard_charge')
 
@@ -118,16 +129,22 @@ def convert_chunk(chunk, ccn):
         df_mid['hcpcs_cpt'] = None
 
     df_mid.loc[df_mid['hcpcs_cpt'].isnull(), 'hcpcs_cpt'] = ''
-    df_mid.loc[df_mid['hcpcs_cpt'] == 'WC003', 'code'] = 'WC003'
-    df_mid.loc[df_mid['hcpcs_cpt'] == 'WC003', 'hcpcs_cpt'] = ''
-    df_mid.loc[df_mid['hcpcs_cpt'].str.startswith('CS'), 'code'] = df_mid[df_mid['hcpcs_cpt'].str.startswith('CS')]['hcpcs_cpt']
-    df_mid.loc[df_mid['hcpcs_cpt'].str.startswith('CS'), 'hcpcs_cpt'] = ''
+
+    for bad_prefix in ['WC', 'CS', 'ED', 'GO', 'CC', 'JO', 'AA', '1-', 'CI']:
+        df_mid.loc[df_mid['hcpcs_cpt'].str.startswith(bad_prefix), 'code'] = df_mid[df_mid['hcpcs_cpt'].str.startswith(bad_prefix)]['hcpcs_cpt']
+        df_mid.loc[df_mid['hcpcs_cpt'].str.startswith(bad_prefix), 'hcpcs_cpt'] = ''
+
     df_mid.loc[df_mid['hcpcs_cpt'].str.isalpha(), 'code'] = df_mid[df_mid['hcpcs_cpt'].str.isalpha()]['hcpcs_cpt']
-    df_mid['hcpcs_cpt'] = df_mid['hcpcs_cpt'].apply(lambda cpt: '' if len(cpt) != 5 else cpt)
+    df_mid['hcpcs_cpt'] = df_mid['hcpcs_cpt'].apply(lambda cpt: '' if len(cpt) != 5 else cpt.upper())
     df_mid.loc[df_mid['hcpcs_cpt'].str.isalpha(), 'hcpcs_cpt'] = None
     df_mid.loc[df_mid['hcpcs_cpt'] == '', 'hcpcs_cpt'] = None
 
-    df_mid['ndc'] = df_mid['ndc'].apply(lambda ndc: ndc[:-1] if type(ndc) == str and len(ndc) == 14 else ndc)
+    if 'ms_drg' in df_mid.columns:
+        df_mid.loc[df_mid['ms_drg'].isnull(), 'ms_drg'] = ''
+        df_mid.loc[df_mid['ms_drg'] != '', 'ms_drg'] = df_mid[df_mid['ms_drg'] != '']['ms_drg'].apply(lambda drg: str(drg).replace('.0', '').zfill(3))
+    
+    # e.g. 12870-0001-1-99-100
+    df_mid['ndc'] = df_mid['ndc'].apply(lambda ndc: ndc[:13] if type(ndc) == str and len(ndc) >= 14 else ndc)
     
     df_mid['hospital_id'] = ccn
     df_mid['line_type'] = None
@@ -137,6 +154,8 @@ def convert_chunk(chunk, ccn):
         df_mid['code'] = None
     if not 'ms_drg' in df_mid.columns:
         df_mid['ms_drg'] = None
+    if not 'modifiers' in df_mid.columns:
+        df_mid['modifiers'] = None
     df_mid['apr_drg'] = None
     df_mid['eapg'] = None
     df_mid['alt_hcpcs_cpt'] = None
@@ -221,8 +240,16 @@ def main():
         ccn = row.get('ccn')
         app_url = row.get('app_url')
 
-        perform_task(h_f, ccn, app_url, transparency_page)
-    
+        try:
+            perform_task(h_f, ccn, app_url, transparency_page)
+        except Exception as e:
+            print(e)
+
+            try:
+                perform_task(h_f, ccn, app_url, transparency_page)
+            except Exception as e:
+                print(e)
+                
     h_f.close()
     in_f.close()
 
