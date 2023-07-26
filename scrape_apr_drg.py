b6165c75b2c06e6852247ce6779170e4f32f81ae
rl1987 <rl1987@users.noreply.github.com>
2023-07-26 13:06:32 +0300

    Start APR-DRG codes and descriptions

--- scrape_apr_drg.py
+++ scrape_apr_drg.py
@@ -2,11 +2,42 @@
 
 import csv
 from pprint import pprint
+from io import StringIO
 
 import requests
 
+FIELDNAMES = [ "code", "code_type", "description", "url" ]
+
 def main():
-    pass
+    url = "https://apps.3mhis.com/docs/Groupers/All_Patient_Refined_DRG/apr400_DRG_descriptions.txt"
+
+    resp = requests.get(url)
+    print(resp.url)
+
+    buf = StringIO(resp.text)
+
+    csv_reader = csv.DictReader(buf, delimiter="|")
+
+    out_f = open("apr_drg.csv", "w", encoding="utf-8")
+
+    csv_writer = csv.DictWriter(out_f, fieldnames=FIELDNAMES, lineterminator="\n")
+    csv_writer.writeheader()
+
+    for in_row in csv_reader:
+        pprint(in_row)
+
+        out_row = {
+            'code': in_row.get('DRG'),
+            'code_type': 'apr_drg',
+            'description': in_row.get("Long Description"),
+            'url': resp.url
+        }
+
+        pprint(out_row)
+
+        csv_writer.writerow(out_row)
+
+    out_f.close()
 
 if __name__ == "__main__":
     main()
