afe395f5bc384ff803335cd7978af0bf7d6672db
rl1987 <rl1987@users.noreply.github.com>
2023-07-26 13:37:06 +0300

    Scrape HCPCS codes and descriptions

--- scrape_hcpcs.py
+++ scrape_hcpcs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import csv
-from pprint import csv
+from pprint import pprint
 from urllib.parse import urljoin
 
 import requests
+from lxml import html
 
 FIELDNAMES = [ "code", "code_type", "description", "url" ]
 
@@ -13,6 +14,50 @@ def main():
 
     resp0 = requests.get(start_url)
     print(resp0.url)
+
+    dom0 = html.fromstring(resp0.text)
+
+    out_f = open("hcpcs.csv", "w", encoding="utf-8")
+
+    csv_writer = csv.DictWriter(out_f, fieldnames=FIELDNAMES, lineterminator="\n")
+    csv_writer.writeheader()
+
+    for link0 in dom0.xpath('//tr[@class="clickable-row"]//a/@href'):
+        url1 = urljoin(resp0.url, link0)
+
+        resp1 = requests.get(url1)
+        print(resp1.url)
+
+        dom1 = html.fromstring(resp1.text)
+
+        for link1 in dom1.xpath('//a[@class="identifier"]/@href'):
+            url2 = urljoin(resp1.url, link1)
+
+            resp2 = requests.get(url2)
+            print(resp2.url)
+
+            dom2 = html.fromstring(resp2.text)
+
+            code = dom2.xpath('//span[@class="identifier16"]/text()')[0]
+            long_desc = dom2.xpath('//h5/text()')[0].strip()
+            short_desc = dom2.xpath('//tr[./td[contains(text(), "Short Description")]]/td[last()]/text()')[0].strip()
+
+            row = {
+                "code": code,
+                "code_type": "hcpcs_cpt",
+                "description": long_desc,
+                "url": resp2.url
+            }
+
+            pprint(row)
+            csv_writer.writerow(row)
+            
+            row['description'] = short_desc
+
+            pprint(row)
+            csv_writer.writerow(row)
+    
+    out_f.close()
 
 if __name__ == "__main__":
     main()
